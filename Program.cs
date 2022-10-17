2df0dc44656f55d145ee1970fa3bdd5c61ec205c
Vladimir <user@MacBook-Air-user.local>
2022-10-17 22:13:19 +0700

    была лишняя переменная, убрал

--- Program.cs
+++ Program.cs
@@ -2,41 +2,15 @@
 452 -> 11
 82 -> 10
 9012 -> 12 */
-/*Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
-7 -> 28
-4 -> 10
-8 -> 36
-*/
-
-//метод который получает число с консоли
-//ЧИсло цифр
-
-/*void getNumber()
-{
-Console.WriteLine("Введите число ");
-int number = int.Parse(Console.ReadLine()!);
-string numAsString = number.ToString();
-
-int length = numAsString.Length;
-
-Console.WriteLine($"{length}");
-}
-getNumber();*/
-
-
 
 Console.WriteLine("Введите число");
-
 int number = int.Parse(Console.ReadLine()!);
-int count = 0;
 int summ = 0;
-int number1 =0;
-
+int number1 = 0;
 while (number > 0)
 {
-    count = count + 1;
     number1 = number % 10;
-    number = number/10;
+    number = number / 10;
     summ = summ + number1;
 }
 Console.WriteLine($"итого {summ}");
